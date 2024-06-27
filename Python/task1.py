5152f23ee3e6bd42e11c7156cef1164d7c0e0331
Radwa Mohamed <radwamohamedakl29@gmail.com>
2024-06-27 23:01:05 +0300

    Add files via upload

---
+++ Python/task1.py
@@ -0,0 +1,4 @@
+#Write a Python program to count the number 4 in a given list.
+list=[1,4,6,7,4]
+Count=list.count(4)
+print('Count of 4:',Count)
---
+++ Python/task2.py
@@ -0,0 +1,8 @@
+#Write a Python program to test whether a passed letter is a vowel or not.
+vowel=['A','E','I','O','U']
+print("Enter a letter for test: ")
+letter = input()
+if letter in vowel:
+   print("it is avowel")
+else:
+   print("it is not vowel")
