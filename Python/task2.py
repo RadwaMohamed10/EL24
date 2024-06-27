9f6fdec4371dc198e93eca2a01e48fb41f96e7ff
Radwa Mohamed <radwamohamedakl29@gmail.com>
2024-06-27 23:16:08 +0300

    Delete Python/task2.py

--- Python/task2.py
+++
@@ -1,8 +0,0 @@
-#Write a Python program to test whether a passed letter is a vowel or not.
-vowel=['A','E','I','O','U']
-print("Enter a letter for test: ")
-letter = input()
-if letter in vowel:
-   print("it is avowel")
-else:
-   print("it is not vowel")
