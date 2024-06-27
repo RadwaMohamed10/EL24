dda1666f1fb94194ae4545ec4cba91a1733e1353
Radwa Mohamed <radwamohamedakl29@gmail.com>
2024-06-27 23:15:01 +0300

    Add files via upload

---
+++ Python/01-Introduction/task4.py
@@ -0,0 +1,6 @@
+
+#Write a Python program which accepts the radius of a circle from the user and compute the area.
+import math
+from math import pi
+r=float(input("please enter the radius of the given circle:"))
+print("the area of the given circle is:",r*r*pi)
